40d986b6f584cf4b5e8a10144642ba62210522e8
nv300024657 <masombuk4657@fvtc.edu>
2016-09-06 14:54:22 -0500

    updated

---
+++ index.php
@@ -0,0 +1,15 @@
+<doctype html> </doctype>
+<html lang="en">
+<head>
+    <title> Nkululeko's Website</title>
+</head>
+<body>
+<marquee>Nkululeko</marquee>
+<header><h1>Nkululeko's Website</h1></header>
+
+    <nav></nav>
+    <main></main>
+    <footer></footer>
+
+</body>
+</html>
