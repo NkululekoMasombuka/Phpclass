f5f476b153aa70acd382464251f24d91a82539a2
nv300024657 <masombuk4657@fvtc.edu>
2016-09-12 23:08:13 -0500

    updated

--- index.php
+++ index.php
@@ -1,15 +1,51 @@
-<doctype html> </doctype>
-<html lang="en">
-<head>
-    <title> Nkululeko's Website</title>
-</head>
-<body>
-<marquee>Nkululeko</marquee>
-<header><h1>Nkululeko's Website</h1></header>
+<!doctype html>
 
-    <nav></nav>
-    <main></main>
-    <footer></footer>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Victor's Website</title>
+
+    <meta>
+
+</head>
+
+<body>
+
+<header>
+
+    <h1>Victor's  Website</h1>
+
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+
+</header>
+
+<nav>
+
+    <ul>
+
+        <li><a href="/">Home</a></li>
+
+        <li><a href="#">Loop Demo</a></li>
+
+        <li><a href="#">About Us</a></li>
+
+    </ul>
+
+</nav>
+
+<main>
+
+    <img src="images/Nkululeko.jpg" alt="Nkululeko's picture"/>
+
+    <p>I am running for President of the Republic of South Africa.</p>
+
+</main>
+
+<footer>&copy; 2016 Nkululeko Victor Masombuka</footer>
 
 </body>
+
 </html>
